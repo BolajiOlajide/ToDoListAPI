601f2fb173e5d26b575fd0333c46bbf6d8e12693
Bolaji Olajide <bolaji.olajide@andela.com>
2017-06-28 12:46:16 +0100

    manage application & add migrate functionality

---
+++ manage.py
@@ -0,0 +1,30 @@
+"""App management settings defined here."""
+from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell, Server
+
+from app import create_app, db
+from app.models import ToDoList
+
+app = Flask(__name__)
+
+
+app = create_app('development')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    """
+    Create a context for interacting in a shell for the application.
+
+    Import the model objects to enable easy interaction.
+    """
+    return dict(app=app, db=db, ToDoList=ToDoList)
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server())
+
+if __name__ == '__main__':
+    manager.run()
